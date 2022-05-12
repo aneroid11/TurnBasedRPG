ae487d2b16fd38fb31e7fc9931514c2e33dbc736
aneroid11 <a-e-v2003@yandex.by>
2022-05-12 21:11:28 +0300

    buttons are working

--- gamescroll.cpp
+++ gamescroll.cpp
@@ -124,6 +124,9 @@ void GameScroll::display(std::wstring text)
 
     this->textCursorPos += sf::Vector2f(0.0f, textHeight);
 
+    AppearingButton* button = new AppearingButton(L"ДжоДжоДжоДжо", *this->textFont, sf::Vector2f(20, 20), style::clean);
+    this->objectsToDraw.push_back(button);
+
     this->drawScrollUntilUserInput();
 }
 
