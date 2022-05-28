3da5d3152ab0731a8dbd8bb3f96c8a755904e200
aneroid11 <a-e-v2003@yandex.by>
2022-05-28 19:25:22 +0300

    GameScroll logic

--- gamescroll.cpp
+++ gamescroll.cpp
@@ -134,79 +134,6 @@ void GameScroll::drawScrollUntilUserInput()
     }
 }
 
-void GameScroll::display(std::wstring text)
-{
-    TextParameters params;
-    params.characterSize = 30;
-    params.font = this->textFont;
-    params.position = this->textCursorPos;
-    params.string = text.c_str();
-    AppearingText* drawableText = new AppearingText(params);
-
-    float lineSpacing = this->textFont->getLineSpacing(params.characterSize) - params.characterSize;
-    float textHeight = drawableText->getText().getGlobalBounds().height + lineSpacing;
-
-    if ((this->textCursorPos + sf::Vector2f(0.0f, textHeight)).y >= this->window->getSize().y)
-    {
-        this->deleteScreenObjects();
-        this->textCursorPos = sf::Vector2f(10.0f, 0.0f);
-        drawableText->setPosition(this->textCursorPos.x, this->textCursorPos.y);
-    }
-
-    this->objectsToDraw.push_back(drawableText);
-
-    this->textCursorPos += sf::Vector2f(0.0f, textHeight);
-
-    this->gotInputFromUser = false;
-    this->lastShown = GAME_TEXT;
-    this->drawScrollUntilUserInput();
-}
-
-std::wstring GameScroll::getUserChoice(const std::list<std::wstring>& choices)
-{
-    // вывести на экран список возможных вариантов в виде кнопок, считать нажатие на кнопку и вернуть
-    // тот вариант, на который нажал игрок
-
-    std::list<AppearingButton*> buttonsToAdd;
-
-    for (const std::wstring& choice : choices)
-    {
-        AppearingButton* button = new AppearingButton(choice, *this->textFont);
-
-        button->attachObserver(this);
-        buttonsToAdd.push_back(button);
-    }
-
-    const float buttonsBlockHeight = (*buttonsToAdd.begin())->getSize().y * buttonsToAdd.size();
-
-    if (this->textCursorPos.y + buttonsBlockHeight > this->window->getSize().y)
-    {
-        deleteScreenObjects();
-    }
-
-    for (AppearingButton* button : buttonsToAdd)
-    {
-        button->setPosition(sf::Vector2f(this->window->getSize().x / 2,
-                                         this->textCursorPos.y));
-
-        this->textCursorPos.y += button->getSize().y;
-        this->objectsToDraw.push_back(button);
-    }
-
-    this->gotInputFromUser = false;
-    this->lastShown = CHOICE_BUTTON;
-
-    bool foundChoiceInList = false;
-    do
-    {
-        this->drawScrollUntilUserInput();
-
-        foundChoiceInList = (std::find(choices.cbegin(), choices.cend(), this->userChoice) != choices.cend());
-    } while (!foundChoiceInList);
-
-    return this->userChoice;
-}
-
 std::wstring GameScroll::displayAndWaitForChoice(const ScreenTextList& screenObjects)
 {
     this->deleteScreenObjects();
@@ -241,6 +168,18 @@ std::wstring GameScroll::displayAndWaitForChoice(const ScreenTextList& screenObj
     } while (!foundChoiceInList);
 
     return this->userChoice;
+}
+
+void GameScroll::addScreenText(const std::array<std::wstring, 2> &text)
+{
+    this->addedScreenText.push_back(text);
+}
+
+std::wstring GameScroll::displayAddedObjectsAndChoice()
+{
+    std::wstring choice = this->displayAndWaitForChoice(this->addedScreenText);
+    this->addedScreenText.clear();
+    return choice;
 }
 
 void GameScroll::update(const std::wstring msgFromButton)
--- gamescroll.h
+++ gamescroll.h
@@ -14,19 +14,16 @@
 
 typedef std::vector< std::array<std::wstring, 2> > ScreenTextList;
 
-const int GAME_TEXT = 0;
-const int CHOICE_BUTTON = 1;
-
 class GameScroll : public IObserver
 {
 public:
     static GameScroll* getInstance();
     static void deleteInstance();
 
-    void display(std::wstring text);
-    std::wstring getUserChoice(const std::list<std::wstring>& choices);
-
     std::wstring displayAndWaitForChoice(const ScreenTextList& screenObjects);
+
+    void addScreenText(const std::array<std::wstring, 2>& text);
+    std::wstring displayAddedObjectsAndChoice();
 
     void update(const std::wstring msgFromSubject) override;
 
@@ -44,7 +41,6 @@ private:
 
     static GameScroll* instance;
 
-    void handleMousePressedEvent(const sf::Event& event);
     void drawScrollUntilUserInput();
 
     sf::RenderWindow* window = nullptr;
@@ -56,9 +52,10 @@ private:
     bool transferControlToCaller = true;
     sf::Vector2f textCursorPos = sf::Vector2f(10.0f, 0.0f);
 
+    ScreenTextList addedScreenText;
+
     std::wstring userChoice = L"";
     bool gotInputFromUser = false;
-    int lastShown;
 };
 
 #endif // GAMESCROLL_H
